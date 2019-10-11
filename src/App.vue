5ac6818ad66015ba752740b29c0a9f846eeac76a
wuhu <wuhu@itcast.cn>
2019-10-11 10:51:00 +0800

    01-适配
    使用vw实现屏幕的适配

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="box"></div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
   }
 }
 </script>
 
 <style lang="less">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#app{
+  background-color: #f00;
+  width:100vw;
+  height: 100vh;
+  .box{
+    width: 126 / 360 * 100vw;
+    height: 126 / 360 * 100vw;
+    background-color: #00f;
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 引入全局样式
+import '@/styles/reset.less'
 
 Vue.config.productionTip = false
 
